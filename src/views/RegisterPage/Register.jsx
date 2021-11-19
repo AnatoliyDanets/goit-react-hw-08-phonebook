8eec3264890e8b48204deb6f9cad5101b5808707
AnatoliyDanets <danetsxp@gmail.com>
2021-11-20 01:25:26 +0200

    deploy netlify

--- src/views/RegisterPage/Register.jsx
+++ src/views/RegisterPage/Register.jsx
@@ -30,9 +30,6 @@ export default function Register() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.name.value);
-    console.log(e.target.email.value);
-    console.log(e.target.password.value);
     dispatch(authUser({ name, email, password }));
 
     reset();
